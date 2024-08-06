c25d52794bb79e75969abd2d4dc562ce8706c37c
user <mirabbosegamberdiyev7@gmail.com>
2024-08-07 00:21:20 +0500

    Swagger auto ochish uchu qo'shildi

--- src/main/java/fido/uz/mohir_dev_jpa/MohirDevJpaApplication.java
+++ src/main/java/fido/uz/mohir_dev_jpa/MohirDevJpaApplication.java
@@ -1,6 +1,8 @@
 package fido.uz.mohir_dev_jpa;
 
+import fido.uz.mohir_dev_jpa.configuration.SwaggerUIOpener;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -8,21 +10,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+import java.awt.*;
+import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.time.Duration;
 
 @SpringBootApplication
 public class MohirDevJpaApplication {
-
-    @Value("${http.proxy.ip}")
-    private String PROXY_SERVER_HOST;
-
-    @Value("${http.proxy.port}")
-    private Integer PROXY_SERVER_PORT;
-
-    @Value("${http.use.proxy}")
-    private Boolean useProxy;
+//
+//    @Value("${swagger.ui-url}")
+//    private  String swaggerUiUrl;
 
     public static void main(String[] args) {
         SpringApplication.run(MohirDevJpaApplication.class, args);
@@ -31,10 +31,6 @@ public class MohirDevJpaApplication {
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
-        if (useProxy) {
-            Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(PROXY_SERVER_HOST, PROXY_SERVER_PORT));
-            requestFactory.setProxy(proxy);
-        }
 
         RestTemplate restTemplate = builder
                 .setConnectTimeout(Duration.ofSeconds(60))
@@ -45,4 +41,84 @@ public class MohirDevJpaApplication {
 
         return restTemplate;
     }
+    @Bean
+    public CommandLineRunner openSwaggerUI(SwaggerUIOpener swaggerUIOpener) {
+        return args -> swaggerUIOpener.openSwaggerUI();
+    }
+
+//    @Bean
+//    public CommandLineRunner openSwaggerUI() {
+//        return args -> {
+//            try {
+//                URI uri = new URI(swaggerUiUrl);
+//
+//                // Detect the operating system
+//                String os = System.getProperty("os.name").toLowerCase();
+//
+//                // Execute the command based on the operating system
+//                if (os.contains("win")) {
+//                    // Windows
+//                    String command = "rundll32 url.dll,FileProtocolHandler " + uri.toString();
+//                    Runtime.getRuntime().exec(command);
+//                } else if (os.contains("mac")) {
+//                    // macOS
+//                    String command = "open " + uri.toString();
+//                    Runtime.getRuntime().exec(command);
+//                } else if (os.contains("nix") || os.contains("nux") || os.contains("nux")) {
+//                    // Linux
+//                    String command = "xdg-open " + uri.toString();
+//                    Runtime.getRuntime().exec(command);
+//                } else {
+//                    throw new UnsupportedOperationException("Unsupported operating system: " + os);
+//                }
+//            } catch (IOException | URISyntaxException e) {
+//                e.printStackTrace();
+//            }
+//        };
+//    }
+
+//    public static void openSwaggerUI() {
+//        try {
+//            String url = "http://localhost:9098/swagger-ui/index.html";
+//            String chromePath = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe";
+//
+//            ProcessBuilder processBuilder = new ProcessBuilder(chromePath, url);
+//            processBuilder.start();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+//    @Value("${http.proxy.ip}")
+//    private String PROXY_SERVER_HOST;
+//
+//    @Value("${http.proxy.port}")
+//    private Integer PROXY_SERVER_PORT;
+//
+//    @Value("${http.use.proxy}")
+//    private Boolean useProxy;
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(MohirDevJpaApplication.class, args);
+//    }
+//
+//    @Bean
+//    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+//        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+//        if (useProxy) {
+//            Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(PROXY_SERVER_HOST, PROXY_SERVER_PORT));
+//            requestFactory.setProxy(proxy);
+//        }
+//
+//        RestTemplate restTemplate = builder
+//                .setConnectTimeout(Duration.ofSeconds(60))
+//                .setReadTimeout(Duration.ofSeconds(60))
+//                .build();
+//
+//        restTemplate.setRequestFactory(requestFactory);
+//
+//        return restTemplate;
+//    }
+
+
 }
---
+++ src/main/java/fido/uz/mohir_dev_jpa/configuration/SwaggerUIOpener.java
@@ -0,0 +1,48 @@
+package fido.uz.mohir_dev_jpa.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Component
+public class SwaggerUIOpener {
+
+    @Value("${swagger.ui-url}")
+    private String swaggerUiUrl;
+
+    public void openSwaggerUI() {
+        if (swaggerUiUrl == null || swaggerUiUrl.isEmpty()) {
+            System.err.println("Swagger UI URL is not set. Please check the application.yml file.");
+            return;
+        }
+
+        try {
+            URI uri = new URI(swaggerUiUrl);
+
+            // Detect the operating system
+            String os = System.getProperty("os.name").toLowerCase();
+
+            // Execute the command based on the operating system
+            if (os.contains("win")) {
+                // Windows
+                String command = "rundll32 url.dll,FileProtocolHandler " + uri.toString();
+                Runtime.getRuntime().exec(command);
+            } else if (os.contains("mac")) {
+                // macOS
+                String command = "open " + uri.toString();
+                Runtime.getRuntime().exec(command);
+            } else if (os.contains("nix") || os.contains("nux")) {
+                // Linux
+                String command = "xdg-open " + uri.toString();
+                Runtime.getRuntime().exec(command);
+            } else {
+                throw new UnsupportedOperationException("Unsupported operating system: " + os);
+            }
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+}
